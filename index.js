141a48496a3cbd4c29b82ab02cbce2d349892c2b
piwiit <bouillaux@hotmail.fr>
2022-07-04 22:10:02 +0200

    add fetch logical

---
+++ index.js
@@ -0,0 +1,24 @@
+let countryData = [];
+
+const fetchCountry = async (search) => {
+  if (search === '') {
+    await fetch('https://restcountries.com/v3.1/all')
+      .then((res) => res.json())
+      .then((data) => (countryData = data));
+    alert('search === null');
+  } else {
+    await fetch('https://restcountries.com/v3.1/name/' + search)
+      .then((res) => res.json())
+      .then((data) => (countryData = data));
+  }
+  console.log(countryData);
+};
+
+inputSearch.addEventListener('input', (e) => {
+  fetchCountry(e.target.value);
+});
+
+inputSearch.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetchCountry();
+});
