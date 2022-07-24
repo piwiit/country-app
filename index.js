a066a7433cc0332f17c9c37536bd7da866303fb4
piwiit <bouillaux@hotmail.fr>
2022-07-24 19:35:15 +0200

    add ascendingSort function and his event

--- index.js
+++ index.js
@@ -5,33 +5,41 @@ let countriesData;
 
 const fetchcountries = async () => {
   countriesData = await fetch('https://restcountries.com/v3.1/all').then((res) => res.json());
-
-  console.log(countriesData);
+  countriesDisplay();
 };
 
-const countriesDisplay = async () => {
-  await fetchcountries();
-  countriesData.length = rangeValue.value;
-
+const countriesDisplay = () => {
   container.innerHTML = countriesData
-    .filter((countrie) =>
-      countrie.translations.fra.common.toLowerCase().includes(inputSearch.value.toLowerCase())
+    .filter((country) =>
+      country.translations.fra.common.toLowerCase().includes(inputSearch.value.toLowerCase())
     )
     .map(
-      (countrie) =>
+      (country) =>
         `
           <div class='card'>
-            <img src='${countrie.flags.svg}'>
-            <h2>${countrie.translations.fra.common}</h2>
-            <h3>${countrie.capital}</h3>
-            <p>Population : ${countrie.population.toLocaleString()} habitants</p>            
+            <img src='${country.flags.svg}'>
+            <h2>${country.translations.fra.common}</h2>
+            <h3>${country.capital}</h3>
+            <p>Population : ${country.population.toLocaleString()} habitants</p>            
           </div>
         `
     )
     .join('');
 };
 
-input.addEventListener('change', () => countriesDisplay());
+const ascendingSort = () => {
+  countriesData.sort((a, b) => a.population - b.population);
+};
+
+minToMax.addEventListener('click', () => {
+  ascendingSort();
+  countriesDisplay();
+});
+
+input.addEventListener('change', () => {
+  countriesDisplay();
+  console.log(countriesData);
+});
 inputSearch.addEventListener('input', () => countriesDisplay());
 
-countriesDisplay();
+fetchcountries();
